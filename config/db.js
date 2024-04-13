604bd10d2d029461d83f252a5d136bdff1db8c01
creatingdigitalvalue <creatingdigitalvalue@gmail.com>
2024-04-13 21:40:51 +0200

    updates .env and the db.js with the env from heroku

--- config/db.js
+++ config/db.js
@@ -1,11 +1,15 @@
 const { Sequelize } = require('sequelize');
 require('dotenv').config();
 
-const sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASSWORD, {
-  host: process.env.DB_HOST,
-  port: process.env.DB_PORT,
+
+// Create a Sequelize instance with environment variables
+const sequelize = new Sequelize({
   dialect: 'postgres',
-  logging: true
+  host: process.env.DB_HOST || process.env.LO_DB_HOST,
+  database: process.env.DB_NAME || process.env.LO_DB_NAME,
+  username: process.env.DB_USER || process.env.LO_DB_USER,
+  password: process.env.DB_PASSWORD || process.env.LO_DB_PASSWORD,
+  port: process.env.DB_PORT || process.env.LO_DB_PORT
 });
 
 // Test the connection
