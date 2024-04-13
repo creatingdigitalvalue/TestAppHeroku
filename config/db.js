f8ac044452316a4e06deedd7fe415f19438afc05
creatingdigitalvalue <creatingdigitalvalue@gmail.com>
2024-04-13 23:04:50 +0200

    Update db.js

--- config/db.js
+++ config/db.js
@@ -1,16 +1,32 @@
 const { Sequelize } = require('sequelize');
 require('dotenv').config();
 
+let sequelize;
 
-// Create a Sequelize instance with environment variables
-const sequelize = new Sequelize({
-  dialect: 'postgres',
-  host: process.env.DB_HOST || process.env.LO_DB_HOST,
-  database: process.env.DB_NAME || process.env.LO_DB_NAME,
-  username: process.env.DB_USER || process.env.LO_DB_USER,
-  password: process.env.DB_PASSWORD || process.env.LO_DB_PASSWORD,
-  port: process.env.DB_PORT || process.env.LO_DB_PORT
-});
+if (process.env.DATABASE_URL) {
+  // If running on Heroku, use the DATABASE_URL environment variable
+  sequelize = new Sequelize(process.env.DATABASE_URL, {
+    dialect: 'postgres',
+    protocol: 'postgres',
+    logging: false, // Disable logging (you can enable it for debugging)
+    dialectOptions: {
+      ssl: {
+        require: true,
+        rejectUnauthorized: false // This line is important on Heroku
+      }
+    }
+  });
+} else {
+  // If running locally or in other environments, use the provided credentials
+  sequelize = new Sequelize({
+    dialect: 'postgres',
+    host: process.env.DB_HOST || process.env.LO_DB_HOST,
+    database: process.env.DB_NAME || process.env.LO_DB_NAME,
+    username: process.env.DB_USER || process.env.LO_DB_USER,
+    password: process.env.DB_PASSWORD || process.env.LO_DB_PASSWORD,
+    port: process.env.DB_PORT || process.env.LO_DB_PORT
+  });
+}
 
 // Test the connection
 sequelize.authenticate()
